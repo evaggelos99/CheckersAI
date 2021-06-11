6aea44e202ef00d100d1e185b0623e9044a6cf95
Evangelos <evaggelosg99@gmail.com>
2021-06-11 19:24:25 +0300

    See Notes 📝 🔧

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from typing import SupportsComplex
 import numpy as np
 import copy
+import math
+import time
+import random
 
 
 def getInitialBoard():
@@ -56,11 +58,11 @@ def gameIsSolved(board):
                 sum2+=1
     
     if sum1==0:
-        return True
+        return True,1
     elif sum2==0:
-        return True
+        return True,2
     else:
-        return False
+        return False,None
     pass
 
 
@@ -69,48 +71,184 @@ This function only checks for blanks
 still havent reached more into it
 '''
 def getPossibleMoves(player,board):
-    board = copy.deepcopy(board)
-
+    copyBoard = copy.deepcopy(board)
+    moves = []
+    
     if player==2:
         # we know that this is player 2, and we check the upper board with different kinds of bounds
         for x in range(8):
             for y in range(8):
                 # without queens or kings whatever
-                if board[x][y] == player:
-                    # now we check Diagonal bounds
-
-                    # this is the left bound
-                    print("we are currently in", x, y, ", this is player: ", player)
-                    if isItBeingBlocked(x,y,board,"LEFT",player):
-                        print("left bound okay")
-
-                    if isItBeingBlocked(x,y,board,"RIGHT",player):
-                        print("right bound okay")
-                    print
-    elif player==1:
+                if board[x][y] == 2:
+                    # if we have an attack availalble
+                    if isAttackAvailable(x,y,board,2) !=[]:
+                        listOfDirection = whereIsTheAttack(x,y,board,2)
+                        if listOfDirection!=[]:
+                            for direction in listOfDirection:
+                                if direction=="LEFT":
+                                    # we know that we can jump there so we check if there is another attack there
+                                    # manipulate the board
+                                    newBoard = copy.deepcopy(copyBoard)
+                                    newBoard[x][y]=0
+                                    newBoard[x+1][y-1]=0
+                                    newBoard[x+2][y-2]=2
+                                    newX,newY = x+2,y-2
+                                    #print(newBoard)
+                                    if isAttackAvailable(x+2,y-2,newBoard,2) != []:
+                                        newListOfDirection = whereIsTheAttack(x+2,y-2,newBoard,2)
+                                        if newListOfDirection!=[]:
+                                            for direction in newListOfDirection:
+                                                if direction=="LEFT":
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX+1][newY-1]=0
+                                                    newBoard[newX+2][newY-2]=2
+                                                    moves.append(copy.deepcopy(newBoard))
+                                                else:
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX+1][newY+1]=0
+                                                    newBoard[newX+2][newY+2]=2
+                                                    moves.append(copy.deepcopy(newBoard))
+                                    else:
+                                        moves.append(copy.deepcopy(newBoard))
+                                else:
+                                    # right
+                                    
+                                    newBoard = copy.deepcopy(copyBoard)
+                                    newBoard[x][y]=0
+                                    newBoard[x+1][y+1]=0
+                                    newBoard[x+2][y+2]=2
+                                    newX,newY = x+2,y+2
+                                    if isAttackAvailable(x+2,y+2,newBoard,2) != []:
+                                        newListOfDirection = whereIsTheAttack(x+2,y+2,newBoard,2)
+                                        if newListOfDirection!=[]:
+                                            for direction in newListOfDirection:
+                                                if direction=="LEFT":
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX+1][newY-1]=0
+                                                    newBoard[newX+2][newY-2]=2
+                                                    moves.append(copy.deepcopy(newBoard))
+                                                else:
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX+1][newY+1]=0
+                                                    newBoard[newX+2][newY+2]=2
+                                                    moves.append(copy.deepcopy(newBoard))
+                                    else:
+                                        moves.append(copy.deepcopy(newBoard))
+                    # no attack simple move
+                    else:
+                        # left
+                        
+                        if isItInBounds(x+1,y-1) and board[x+1][y-1]==0:
+                            edit = copy.deepcopy(board)
+                            edit[x][y]=0
+                            edit[x+1][y-1]=2
+                            moves.append(edit)
+                        # right
+                        if isItInBounds(x+1,y+1) and board[x+1][y+1]==0:
+                            edit = copy.deepcopy(board)
+                            edit[x][y]=0
+                            edit[x+1][y+1]=2
+                            moves.append(edit)
+    elif player==1:   
         for x in range(8):
             for y in range(8):
-                # without queens or kings whatever
-                if board[x][y] == player:
-                    # now we check Diagonal bounds
+                # without queens or kings whatever   
+                if board[x][y] == 1:
+                    if isAttackAvailable(x,y,board,1) !=[]:
+                        listOfDirection = whereIsTheAttack(x,y,board,1)
+                        if listOfDirection!=[]:
+                            for direction in listOfDirection:
+                                if direction=="LEFT":
+                                    # we know that we can jump there so we check if there is another attack there
+                                    # manipulate the board
+                                    newBoard = copy.deepcopy(copyBoard)
+                                    newBoard[x][y]=0
+                                    newBoard[x-1][y-1]=0
+                                    newBoard[x-2][y-2]=1
+                                    newX, newY= x-2, y-2
+                                    if isAttackAvailable(x-2,y-2,newBoard,1) != []:
+                                        newListOfDirection = whereIsTheAttack(x-2,y-2,newBoard,1)
+                                        if newListOfDirection!=[]:
+                                            for direction in newListOfDirection:
+                                                if direction=="LEFT":
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX-1][newY-1]=0
+                                                    newBoard[newX-2][newY-2]=1
+                                                else:
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX-1][newY-1]=0
+                                                    newBoard[newX-2][newY-2]=1
+                                    else:
+                                        moves.append(newBoard)
+                                else:
+                                    newBoard = copy.deepcopy(copyBoard)
+                                    newBoard[x][y]=0
+                                    newBoard[x-1][y+1]=0
+                                    newBoard[x-2][y+2]=1
+                                    newX,newY= x-2,y+2
+                                    if isAttackAvailable(x-2,y+2,newBoard,1) != []:
+                                        newListOfDirection = whereIsTheAttack(x-2,y-2,newBoard,1)
+                                        if newListOfDirection!=[]:
+                                            for direction in newListOfDirection:
+                                                if direction=="LEFT":
+                                                    newBoard[newX][newY]=0
+                                                    newBoard[newX-1][newY+1]=0
+                                                    newBoard[newX-2][newY+2]=1
+                                                else:
+                                                    newBoard[newY][newY]=0
+                                                    newBoard[newX-1][newY+1]=0
+                                                    newBoard[newX-2][newY+2]=1
+                                    else:
+                                        moves.append(newBoard)
+                    # no attack simple move
+                    else:
+                        # left
+                        if isItInBounds(x-1,y-1) and board[x-1][y-1]==0:
+                            edit = copy.deepcopy(board)
+                            edit[x][y]=0
+                            edit[x-1][y-1]=1
+                            moves.append(edit)
+                        # right
+                        if isItInBounds(x-1,y+1) and board[x-1][y+1]==0:
+                            edit = copy.deepcopy(board)
+                            edit[x][y]=0
+                            edit[x-1][y+1]=1
+                            moves.append(edit)
+    return moves
 
-                    # this is the left bound
-                    print("we are currently in", x, y, ", this is player: ", player)
-                    if isItBeingBlocked(x,y,board,"LEFT",player):
-                        print("left bound okay")
-
-                    # right bound 
-                    if isItBeingBlocked(x,y,board,"RIGHT",player):
-                        print("right bound okay")
-                    print
-
-    pass
 
 
 def isAttackAvailable(i,j,board,player):
+    listOfAttackAvailable = []
+    if player==1:
+        # direction left
+        if isItInBounds(i-1, j-1) and board[i-1][j-1]==2  and isItInBounds(i-2,j-2) and board[i-2][j-2]==0:
+            listOfAttackAvailable.append((i-1,j-1))
+            
+            
+        # direction right
+        if isItInBounds(i-1, j+1) and board[i-1][j+1]==2 and isItInBounds(i-2,j+2) and board[i-2][j+2]==0:
+            listOfAttackAvailable.append((i-1,j+1))
+            
         
-    pass
+    elif player==2:
+        # direction left
+        if isItInBounds(i+1,j-1) and board[i+1][j-1]==1  and isItInBounds(i+2,j-2) and board[i+2][j-2]==0:
+            listOfAttackAvailable.append((i+1,j-1))
+        
 
+        # direction right
+        if isItInBounds(i+1, j+1) and board[i+1][j+1]==1 and isItInBounds(i+2,j+2) and board[i+2][j+2]==0:
+            listOfAttackAvailable.append((i+1,j+1))
+    else:
+        return None
+    
+    return listOfAttackAvailable
+
+
+'''
+This function only checks for the array bounds nothing else
+'''
 def isItInBounds(i,j):
     if i>=0 and i<=7:
         if j>=0 and j<=7:
@@ -119,10 +257,28 @@ def isItInBounds(i,j):
     pass
 
 
+def isThereAnAttackOnBoard(board, player):
+    for i in range(8):
+        for j in range(8):
+            if board[i][j]  == 1: # player one
+                listOfAttacks=isAttackAvailable(i,j,board,1)
+                if listOfAttacks != []:
+                    if player == 1:
+                        return True
+                
+            elif board[i][j] == 2: # player two
+                listOfAttacks=isAttackAvailable(i,j,board,2)
+                if listOfAttacks != []:
+                    if player == 2:
+                        return True
+    
+    return []
+
+
 '''
 i and j is used for the current checker piece and directions sets where to look for
 '''
-def isItBeingBlocked(i,j,board, direction, player):
+def isItClearBounds(i,j,board, direction, player):
     if player==2:
         if direction=="LEFT":
             if isItInBounds(i+1,j-1):
@@ -143,33 +299,177 @@ def isItBeingBlocked(i,j,board, direction, player):
                         if (0 == board[i-1][j+1]):
                             return True
     return False
-    pass
 
-def startGame(board):
+def whereIsTheAttack(i,j,board, player):
+    attacks = []
+
+    if player==2:
+        if isItInBounds(i+1,j-1) and board[i+1][j-1] == 1 and isAttackValid(i,j,board,2,"LEFT"):
+            attacks.append("LEFT")
+        if  isItInBounds(i+1,j+1) and board[i+1][j+1]==1 and isAttackValid(i,j,board,2,"RIGHT"):
+            attacks.append("RIGHT")
+        return attacks
+    elif player==1:
+            if isItInBounds(i-1,j-1) and 2 == board[i-1][j-1] and isAttackValid(i,j,board,1,"LEFT"):
+                attacks.append("LEFT")
+            if isItInBounds(i-1,j+1) and 2 == board[i-1][j+1] and isAttackValid(i,j,board,1,"RIGHT"):
+                attacks.append("RIGHT")
+    return attacks
+
+
+def move(player,board):
+    # copiedBoard = copy.deepcopy(board)
+
+    bestScore = -math.inf
+    moves = getPossibleMoves(player, board)
+    moveloc = moves[0]
+    for move in moves:
+        score = alphabeta(move, 5, -math.inf, math.inf, False)
+        if score > bestScore:
+            bestScore = score
+            moveLoc = move
+    return moveLoc
+
+
+def startGame():
+
+    board = getInitialBoard()
+
+    # create player objects
     player = 1
+
     while(True):
+        print("player:",player)
         print(board)
-
-
-
-        break
+        properMove = move(player,board)
+        board = properMove
+        
+        boolis, playerWon = gameIsSolved(board)
+        if boolis :
+            print(playerWon, "Won!")
+            break
+        
+        if player==1:
+            player=2
+        else:
+            player=1
         
 
-        if (gameIsSolved(board)):
-            print
 
 
+
+def evaluationFun(board,player):
+    val,play=gameIsSolved(board)
+    lengthTwo = np.count_nonzero(board == 2)
+    lengthOne = np.count_nonzero(board == 1)
+    if player==1:
+        if val and play==2:
+            return -100 
+        elif val and play==1:
+            return +100 
+        else:
+            return lengthOne-lengthTwo
+    if player==2:
+        
+        if val and play==2:
+            return +100
+        elif val and play==1:
+            return -100
+        else:
+            return lengthTwo-lengthOne
+
+
+    
+
+def isAttackValid(i,j,board,player,dir):
+    if player==1:
+        if dir=="LEFT":
+            if isItInBounds(i-2,j-2) and board[i-2][j-2]==0:
+                return True
+        if dir=="RIGHT":
+            if isItInBounds(i-2,j+2) and board[i-2][j+2]==0:
+                return True
+        pass
+    elif player==2:
+        if dir=="LEFT":
+            if isItInBounds(i+2,j-2) and board[i+2][j-2]==0:
+                return True
+        if dir=="RIGHT":
+            if isItInBounds(i+2,j+2) and board[i+2][j+2]==0:
+                return True
+
+
+'''
+initial call:
+minimax(currentPosition, 3, -∞, +∞, true)
+'''
+def alphabeta(board,depth,alpha,beta,max):
+    player=0
+    if max:
+        player=1
+    else:
+        player=2
+    
+    if gameIsSolved(board) or depth==0:
+        return evaluationFun(board,player)
+    
+    
+    
+    if max:
+        bestValue = -math.inf
+        
+        for move in getPossibleMoves(player,board=board):
+            value = alphabeta(move,depth-1,alpha,beta, False)
+            bestValue = max(bestValue, value)
+            alpha = max(alpha, value)
+            if beta <= alpha:
+                break
+        return bestValue
+        
+    else:
+        smallValue = math.inf
+        for move in getPossibleMoves(player,board):
+            value = alphabeta(move,depth-1,alpha,beta, True)
+            smallValue = min(smallValue, value)
+            beta = min(beta, value)
+            if beta <= alpha:
+                break
+        return smallValue
 
 if __name__ == "__main__":
+    #startGame()
     board = getInitialBoard()
-    print(board)
+    board = np.zeros((8,8))
+    board[0,1]=1
 
-    #startGame(board)
+    board[1,2]=2
+    board[1,4]=2
+    board[1,6]=2
 
-    '''board = np.zeros(shape=(8,8))
+    board[2,1]=2
+    board[2,3]=2
+    board[2,5]=2
+    board[2,7]=1
 
-    board[0][1] = 2
-    board[1][0] = 2
-    board[1][2] = 2
-    print(board)'''
-    getPossibleMoves(1,board)
+    board[3,0]=2
+    board[3,2]=2
+    board[3,4]=1
+    
+    board[4,1]=1
+
+    board[6,1]=1
+    board[6,3]=1
+    board[6,5]=1
+    board[6,7]=1
+
+    board[7,0]=1
+    board[7,2]=1
+    board[7,4]=1
+    board[7,6]=1
+
+    for x in getPossibleMoves(1,board=board):
+        print(x)
+        print()
+        print(board)
+        print()
+
--- notes.md
+++ notes.md
@@ -17,3 +17,130 @@
  [0. 1. 0. 1. 0. 1. 0. 1.]
 
  [1. 0. 1. 0. 1. 0. 1. 0.]  ]
+
+
+ ## useless shit
+
+class Player:
+
+    id=0
+
+    def __init__(self,id):
+        self.id=id
+
+    def printNum(self):
+        print(self.id)
+
+    '''
+    initial call:
+    minimax(currentPosition, 3, -∞, +∞, true)
+    '''
+    def alphabeta(self,board,depth,alpha,beta,max):
+        if gameIsSolved(board) or depth==0:
+            return evaluationFun(board)
+        
+        player=0
+        if max:
+            player=1
+        else:
+            player=2
+        
+        if max:
+            bestValue = -math.inf
+            
+            for move in getPossibleMoves(player,board=board):
+                value = alphabeta(move,depth-1,alpha,beta, False)
+                bestValue = max(bestValue, value)
+                alpha = max(alpha, value)
+                if beta <= alpha:
+                    break
+            return bestValue
+            
+        else:
+            smallValue = math.inf
+            for move in getPossibleMoves(player,board):
+                value = alphabeta(move,depth-1,alpha,beta, True)
+                smallValue = min(smallValue, value)
+                beta = min(beta, value)
+                if beta <= alpha:
+                    break
+            return smallValue
+
+
+
+    def move(self,board):
+        pass
+
+
+
+
+[[0. 1. 0. 0. 0. 2. 0. 2.]
+ [0. 0. 2. 0. 2. 0. 2. 0.]
+ [0. 2. 0. 2. 0. 2. 0. 1.]
+ [2. 0. 2. 0. 1. 0. 0. 0.]
+ [0. 1. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 1. 0. 1. 0. 1. 0. 1.]
+ [1. 0. 1. 0. 1. 0. 1. 0.]]
+
+
+    board = getInitialBoard()
+    board = np.zeros((8,8))
+    board[0,1]=1
+    board[0,5]=2
+    board[0,7]=2
+
+    board[1,2]=2
+    board[1,4]=2
+    board[1,6]=2
+
+    board[2,1]=2
+    board[2,3]=2
+    board[2,5]=2
+    board[2,7]=1
+
+    board[3,0]=2
+    board[3,2]=2
+    board[3,4]=1
+    
+    board[4,1]=1
+
+    board[6,1]=1
+    board[6,3]=1
+    board[6,5]=1
+    board[6,7]=1
+
+    board[7,0]=1
+    board[7,2]=1
+    board[7,4]=1
+    board[7,6]=1
+
+
+
+    board = np.zeros((8,8))
+
+    board[1][3]=2
+    board[2][2]=1
+    board[4][2]=1
+    print(board)
+    print()
+    moves = getPossibleMoves(1,board)
+    for move in moves:
+        print(move)
+        print()
+
+
+
+## Changes 1.1
+
+* possible moves 
+    * only allows 2 jumps this is wrong but its a start so i can get a minimum viable playable game
+    * more jumps will be added
+    * king promotion will happen aswell
+
+* evaluation function 
+    * is bare bones
+    * paterns of checkers will be added later
+
+* optimization 
+    * this will be added when I have a better evaluation function and understand the game better and redo possible moves fun
